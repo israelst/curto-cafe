2d764d03e91572f1e8b4b927d8c009c439626561
Israel Teixeira <israelst@gmail.com>
2012-09-06 19:24:47 -0300

    Extract script part from GoogleSpreadsheet client

--- downloader/GoogleSpreadsheets.py
+++ downloader/GoogleSpreadsheets.py
@@ -37,12 +37,3 @@ class Client:
         }
         response = requests.get(url, params=params, headers=headers)
         return response
-
-if __name__ == "__main__":
-    from config.gdata import connection_data
-
-    spreadsheet_key = "0AsmXhe3Jaw0hdF83S2RGRHhpQzBsbkN5SGRWZ3NLcVE"
-    gs = Client(*connection_data)
-    response = gs.download(spreadsheet_key, 38)
-    with open('file.ods', 'w') as f:
-        f.write(response.content)
