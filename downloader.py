686fa23aee82efa318596888ab450c263a88898f
Israel Teixeira <israelst@gmail.com>
2012-09-06 20:50:34 -0300

    Remove repetition of file format

--- downloader.py
+++ downloader.py
@@ -3,8 +3,10 @@
 from config.gdata import connection_data
 from downloader.GoogleSpreadsheets import Client
 
+
 spreadsheet_key = "0AsmXhe3Jaw0hdF83S2RGRHhpQzBsbkN5SGRWZ3NLcVE"
 gs = Client(*connection_data)
-response = gs.download(spreadsheet_key, 38)
-with open('file.ods', 'w') as f:
+file_format = "ods"
+response = gs.download(spreadsheet_key, 38, file_format)
+with open('file.{}'.format(file_format), 'w') as f:
     f.write(response.content)
