fe6a0ff9af0ab0f39c4256fea56a4ab604de642e
Israel Teixeira <israelst@gmail.com>
2012-09-08 21:56:18 -0300

    It is actually a xlsx file

--- tests/factories.py
+++ tests/factories.py
@@ -2,7 +2,7 @@
 import xlrd
 
 
-workbook = xlrd.open_workbook('tests/example.ods')
+workbook = xlrd.open_workbook('tests/example.xlsx')
 sheet_662 = workbook.sheet_by_name('662')
 date_cell = sheet_662.cell(24, 3)
 time_cell = sheet_662.cell(13, 3)
