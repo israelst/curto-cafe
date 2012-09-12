1d40ac3c16a8c6ef6cdff7216ac5d79751f6b42d
Israel Teixeira <israelst@gmail.com>
2012-09-12 07:14:03 -0300

    Generate a json file to try a cluster visualization

--- json_dump.py
+++ json_dump.py
@@ -5,18 +5,21 @@ import json
 
 
 data = {}
+data2 = []
 with file('output.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     csv_reader.next() #skip header
     for row in csv_reader:
-        print row
         path, sheet, time, temp, flame = row
         print path, sheet, time, temp, flame
         _id = '{},{}'.format(path,sheet)
         data.setdefault(_id, [])
-        #data.setdefault('id', _id)
-        #data[_id].setdefault([])
         data[_id].append({'time': float(time), 'temp': float(temp.replace("'", ""))})
 
-with file('data.js', 'w') as js_file:
-    json.dump(data, js_file)
+        data2.append({'time': float(time), _id: float(temp.replace("'", ""))})
+
+#with file('data.js', 'w') as js_file:
+#    json.dump(data, js_file)
+
+with file('data2.js', 'w') as js_file:
+    json.dump(data2, js_file)
